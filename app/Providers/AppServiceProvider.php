3fbedadc153397d57e159ebb761b53753ca6a2bb
cjmeeks <clifford.j.meeks@gmail.com>
2020-05-25 10:56:46 -0500

    get running with valet

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Schema\Builder; // Import Builder where defaultStringLength method is defined
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,13 +17,8 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
+    function boot()
     {
-        //
+        Builder::defaultStringLength(191); // Update defaultStringLength
     }
 }
